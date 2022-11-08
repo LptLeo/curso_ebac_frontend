832bdb3b4a537e44ba9b514f840c65c678957dfe
Leonardo <lptleo11@gmail.com>
2022-11-08 14:04:01 -0400

    exercicio_html_js - v1.1

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-var input_a = document.querySelector('.a')
-var input_b = document.querySelector('.b')
+var input_a = parseFloat(document.querySelector('.a'))
+var input_b = parseFloat(document.querySelector('.b'))
 
-function AtualizadorDeValor() {
-    var input_a = document.querySelector('.a')
-    var input_b = document.querySelector('.b')
+function maior() {
+    parseFloat(document.querySelector('.a'))
+    parseFloat(document.querySelector('.b'))
+    if (document.querySelector('.b').value > document.querySelector('.a').value) {
+        console.log('B maior que A!')
+    } else if (document.querySelector('.b').value == document.querySelector('.a').value) {
+        console.log('B é igual á A!')
+    } else {
+        console.log('B é menor que A!')
+    }
+    console.log(`input_a valor = ${document.querySelector('.a').value}`)
+    console.log(`input_b valor = ${document.querySelector('.b').value}`)
 }
 
-addEventListener('input', AtualizadorDeValor)
+addEventListener('input', maior)
 addEventListener('submit', function(e) {
     console.log('Forumlário enviado!')
     e.preventDefault()
-    if (input_b.value > input_a.value) {
+    if (document.querySelector('.b').value > document.querySelector('.a').value) {
         document.querySelector('.success-msg').style.display = 'block'
         document.querySelector('.error-msg').style.display = ''
-    } else if (input_a.value == input_b.value) {
+    } else if (document.querySelector('.b').value == document.querySelector('.a').value) {
         document.querySelector('.error-msg').style.display = 'block'
         document.querySelector('.success-msg').style.display = ''
     } else {
